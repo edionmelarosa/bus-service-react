74e3a994e9967d5c9be979d29fdd6af3d51c5785
Espiridion Larosa <edion.larosa@gmail.com>
2021-04-25 12:46:59 +0800

    calculate next bus arrival

--- src/BusesList.js
+++ src/BusesList.js
@@ -1,9 +1,24 @@
 import './App.css';
 import { React, useEffect, useState } from 'react';
 import Axios from './axios';
+import moment from 'moment';
 
 function calculateArrivalTiming(arrivalTime) {
-  return arrivalTime
+  const [hour, minutes] = arrivalTime.split(':');
+  let schedule = moment()
+  schedule.set('hour', hour)
+  schedule.set('m', minutes)
+
+  return schedule.fromNow()
+}
+
+function formatArrivalTime(arrivalTime) {
+  const [hour, minutes] = arrivalTime.split(':');
+  let schedule = moment()
+  schedule.set('hour', hour)
+  schedule.set('m', minutes)
+
+  return schedule.format('h:mm A');
 }
 
 export default function BusesList(props) {
@@ -31,11 +46,12 @@ export default function BusesList(props) {
   }, [props.bus._id])
   return (
     <div className="Bus-List">
-      <h3>Available Buses</h3>
+      <h3>Buses</h3>
       {buses.map(item => (
         <div className="Bus-Stop-Item" key={item.name}>
           <h5>{item.name}</h5>
-          <small>Arrival: {calculateArrivalTiming(item.arrivalTime)} </small>
+          <small>Arrival: {formatArrivalTime(item.arrivalTime)}</small> <br />
+          <small>{calculateArrivalTiming(item.arrivalTime)}</small>
         </div>
       ))}
     </div>
