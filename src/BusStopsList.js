c20a0c4d5b5ff4500bd84e3919d047a4a53ccb7f
Espiridion Larosa <edion.larosa@gmail.com>
2021-04-25 11:58:19 +0800

    make buses show when bus stop item is click

--- src/BusStopsList.js
+++ src/BusStopsList.js
@@ -1,15 +1,29 @@
 import './App.css';
 import { React, useEffect, useState } from 'react';
 import Axios from './axios';
+import BusesList from './BusesList';
+
+function ShowBusLists(props) {
+  if (!props.bus) {
+    return null;
+  }
+
+  return (
+    <div className="Active-Bus">
+      <BusesList bus={props.bus} />
+    </div>
+  )
+}
 
 export default function BusStopsList() {
-  const [BusStops, SetBusStops] = useState([]);
+  const [busStops, setBusStops] = useState([]);
+  const [activeBusStop, setActiveBusStop] = useState();
 
   useEffect(() => {
     async function getBuStops() {
       try {
         const response = await Axios.get('/bus-stops');
-        SetBusStops(response.data)
+        setBusStops(response.data)
       } catch (error) {
         alert('Error Pulling Bus Stops.');
       }
@@ -18,12 +32,16 @@ export default function BusStopsList() {
     getBuStops()
   }, [])
   return(
+    <div>
       <div className="Bus-Stop">
-        { BusStops.map(item => (
-          <div className="Bus-Stop-Item" key={item._id}>
+        { busStops.map(item => (
+          <div className="Bus-Stop-Item" key={item._id} onClick={() => setActiveBusStop(item)}>
             <strong>{item.name}</strong>
           </div>
         ))}
       </div>
+
+      <ShowBusLists bus={activeBusStop}/>
+    </div>
   ) 
 }
--- src/BusesList.js
+++ src/BusesList.js
@@ -1,23 +1,43 @@
 import './App.css';
-import { React, Component } from 'react';
+import { React, useEffect, useState } from 'react';
+import Axios from './axios';
 
-class BusesList extends Component {
-  render() {
-    return (
-      <div className="Bus-List">
-        {this.props.buses.map(item => (
-          <div className="Bus-Item" key={item.name}>
-            <h5>{item.name}</h5>
-            <small>Arrival: <strong>{this.calculateArrivalTiming('10mins')}</strong> </small>
-          </div>
-        ))}
-      </div>
-    )
-  }
-
-  calculateArrivalTiming(time) {
-    return time;
-  }
+function calculateArrivalTiming(arrivalTime) {
+  return arrivalTime
 }
 
-export default BusesList;
+export default function BusesList(props) {
+  const [buses, setBuses] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    async function getBuses() {
+      try {
+        const response = await Axios.get(`/bus-stops/${props.bus._id}`);
+        setBuses(response.data.buses)
+      } catch (error) {
+        console.log(error)
+        alert('Error Pulling Buses.');
+      }
+    }
+
+    if (mounted) {
+      getBuses()
+    }
+
+    return () => {
+      mounted = false;
+    }
+  }, [props.bus._id])
+  return (
+    <div className="Bus-List">
+      <h3>Available Buses</h3>
+      {buses.map(item => (
+        <div className="Bus-Stop-Item" key={item.name}>
+          <h5>{item.name}</h5>
+          <small>Arrival: {calculateArrivalTiming(item.arrivalTime)} </small>
+        </div>
+      ))}
+    </div>
+  )
+}
